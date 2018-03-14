da9440fc2c6954d1c033760b072a62fd18561231
Glorityy <37379429+Glorityy@users.noreply.github.com>
2018-03-14 22:20:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const Discord = require("discord.js");
 
-var bot = new Discord.Client();
-
-bot.on("ready", function(){
-    bot.user.setGame("HyneriaUHC |!help");
-    console.log("Le bot a bien été connecté");
-})
-
-bot.on('message', function (message){
-  if(message.content === '!help'){
-    message.channel.sendMessage("Aide :");
-    message.channel.sendMessage("!ip");
-    message.channel.sendMessage("!mumble");
-    message.channel.sendMessage("!boutique");
-    message.channel.sendMessage("!discord");
-  }
-})
