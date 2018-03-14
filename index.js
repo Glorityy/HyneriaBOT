82cc12a29ee278848ed85e6b8f5aebfd41e8229e
Glorityy <37379429+Glorityy@users.noreply.github.com>
2018-03-14 18:27:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ bot.on("ready", function(){
 
 bot.on('message', function (message){
   if(message.content === '!help'){
-    message.channel.sendMessage('Aide :');
-    message.channel.sendMessage('!ip');
-    message.channel.sendMessage('!mumble');
-    message.channel.sendMessage('!boutique');
-    message.channel.sendMessage('!discord');
+    message.channel.sendMessage("Aide :");
+    message.channel.sendMessage("!ip");
+    message.channel.sendMessage("!mumble");
+    message.channel.sendMessage("!boutique");
+    message.channel.sendMessage("!discord");
   }
 })
